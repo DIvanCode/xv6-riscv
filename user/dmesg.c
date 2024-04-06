14fb462183672078126c8967358f54105c8ccf43
DIvanCode <DIvanCode@gmail.com>
2024-04-06 21:24:20 +0300

    fixes in tests

--- user/dmesg.c
+++ user/dmesg.c
@@ -6,6 +6,19 @@
 
 int
 main(int argc, char *argv[]) {
+  if (argc == 2 && strcmp(argv[1], "go") == 0) {
+    // test buffer overflow (!with QNPAGES=1 in param.h!)
+    for (int i = 0; i < 100; ++i) {
+      int pid = fork();
+      if (pid == 0) {
+        exec("vm", argv);
+        exit(0);
+      } else {
+        wait(&pid);
+      }
+    }
+  }
+
   char mesg[SSS];
   if (dmesg(mesg, SSS) < 0) {
     printf("error: dmesg\n");

6e0b25b13400bb1ebc5465855dcef34bd5f326a6
DIvanCode <divancode@gmail.com>
2024-05-06 16:24:47 +0300

    optimized test

--- user/test.c
+++ user/test.c
@@ -6,6 +6,10 @@
 #define B 74
 #define M 998244353
 
+#define BUF_SIZE 1024 * 4
+
+int buf[BUF_SIZE];
+
 int main(int argc, char *argv[]) {
   if (argc != 3) {
     fprintf(2, "test: wrong format (<size> <x0>)\n");
@@ -27,14 +31,19 @@ int main(int argc, char *argv[]) {
   }
 
   int f = open(argv[1], O_RDWR | O_CREATE), wrote_bytes = 0, x = x0;
+  int p = 0;
   for (int i = 0; i < size / 4; ++i) {
-    int cur = write(f, &x, 4);
-    wrote_bytes += cur;
-    if (cur != 4)
-      break;
-    // if ((i + 1) % 10000 == 0)
-    //   fprintf(2, "wrote %d\n", wrote_bytes);
+    buf[p++] = x;
     x = ((uint64) A * x + B) % M;
+
+    if (p == BUF_SIZE || i == size / 4 - 1) {
+      int cur = write(f, buf, p * 4);
+      wrote_bytes += cur;
+      fprintf(2, "wrote %d\n", wrote_bytes);
+      if (cur != p * 4)
+        break;
+      p = 0;
+    }
   }
   close(f);
 
@@ -43,16 +52,17 @@ int main(int argc, char *argv[]) {
   f = open(argv[1], O_RDONLY);
   x = x0;
   int read_bytes = 0, corrupted_bytes = 0;
-  for (int i = 0; i < size / 4; ++i) {
-    int y, cur = read(f, &y, 4);
-    read_bytes += cur;
-    if (cur != 4)
+  while (1) {
+    int cur = read(f, buf, BUF_SIZE * 4);
+    if (cur == 0)
       break;
-    if (x != y)
-      corrupted_bytes++;
-    // if ((i + 1) % 10000 == 0)
-    //  fprintf(2, "read %d\n", read_bytes);
-    x = ((uint64) A * x + B) % M;
+    read_bytes += cur;
+    printf("read %d bytes\n", read_bytes);
+    for (int j = 0; j < cur / 4; ++j) {
+      if (x != buf[j])
+        corrupted_bytes++;
+      x = ((uint64) A * x + B) % M;
+    }
   }
   close(f);
 

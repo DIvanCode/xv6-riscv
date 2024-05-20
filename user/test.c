327e8d3ea977568b7ee7081af43cfa03fa4d5587
DIvanCode <DIvanCode@gmail.com>
2024-05-20 14:57:59 +0300

    fixed tests

--- user/test.c
+++ user/test.c
@@ -7,12 +7,6 @@
 
 void view_tree(char *, int);
 
-void mkfile(char *filename, char *data) {
-  int f = open(filename, O_RDWR | O_CREATE);
-  write(f, data, strlen(data));
-  close(f);
-}
-
 int errors = 0;
 
 void error(int test, int test_case) {
@@ -26,32 +20,89 @@ void ok(int test) {
   errors = 0;
 }
 
+int safe_open(char *filename, int flags) {
+  int f = open(filename, flags);
+  if (f < 0) {
+    fprintf(2, "error: open\n");
+    exit(1);
+  }
+  return f;
+}
+
+void safe_read(int f, char *data, int size) {
+  if (read(f, data, size) < 0) {
+    fprintf(2, "error: read\n");
+    exit(1);
+  }
+}
+
+void safe_write(int f, char *data, int size) {
+  if (write(f, data, size) != size) {
+    fprintf(2, "error: write\n");
+    exit(1);
+  }
+}
+
+void safe_close(int f) {
+  if (close(f) < 0) {
+    fprintf(2, "error: close\n");
+    exit(1);
+  }
+}
+
+void safe_mkfile(char *filename, char *data) {
+  int f = safe_open(filename, O_RDWR | O_CREATE);
+  safe_write(f, data, strlen(data));
+  safe_close(f);
+}
+
+void safe_mkdir(char *name) {
+  if (mkdir(name) < 0) {
+    fprintf(2, "error: mkdir\n");
+    exit(1);
+  }
+}
+
+void safe_symlink(char *target, char *filename) {
+  if (symlink(target, filename) < 0) {
+    fprintf(2, "error: symlink\n");
+    exit(1);
+  }
+}
+
+void safe_readlink(char *filename, char *data) {
+  if (readlink(filename, data) < 0) {
+    fprintf(2, "error: readlink\n");
+    exit(1);
+  }
+}
+
 void test1() {
   int test = 1;
 
   // Arrange
-  mkdir("/test1");
-  mkfile("/test1/a", "data");
+  safe_mkdir("/test1");
+  safe_mkfile("/test1/a", "data");
 
   // Act
-  symlink("/test1/a", "/test1/b");
+  safe_symlink("/test1/a", "/test1/b");
 
   // Assert
   char b_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     b_data[i] = 0;
-  readlink("/test1/b", b_data);
+  safe_readlink("/test1/b", b_data);
   if (strcmp(b_data, "/test1/a") != 0)
     error(test, 1);
 
   char data[16];
   for (int i = 0; i < 16; ++i)
     data[i] = 0;
-  int fb = open("/test1/b", O_RDONLY);
-  read(fb, data, 16);
+  int fb = safe_open("/test1/b", O_RDONLY);
+  safe_read(fb, data, 16);
   if (strcmp(data, "data") != 0)
     error(test, 2);
-  close(fb);
+  safe_close(fb);
 
   ok(test);
 }
@@ -60,28 +111,28 @@ void test2() {
   int test = 2;
 
   // Arrange
-  mkdir("/test2");
-  mkfile("/test2/a", "data");
+  safe_mkdir("/test2");
+  safe_mkfile("/test2/a", "data");
 
   // Act
-  symlink("a", "/test2/b");
+  safe_symlink("a", "/test2/b");
 
   // Assert
   char b_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     b_data[i] = 0;
-  readlink("/test2/b", b_data);
+  safe_readlink("/test2/b", b_data);
   if (strcmp(b_data, "a") != 0)
     error(test, 1);
 
   char data[16];
   for (int i = 0; i < 16; ++i)
     data[i] = 0;
-  int fb = open("/test2/b", O_RDONLY);
-  read(fb, data, 16);
+  int fb = safe_open("/test2/b", O_RDONLY);
+  safe_read(fb, data, 16);
   if (strcmp(data, "data") != 0)
     error(test, 2);
-  close(fb);
+  safe_close(fb);
 
   ok(test);
 }
@@ -90,31 +141,31 @@ void test3() {
   int test = 3;
 
   // Arrange
-  mkdir("/test3");
-  mkfile("/test3/a", "data");
-  mkdir("/test3/d1");
-  mkdir("/test3/d1/d2");
-  mkdir("/test3/d1/d2/d3");
+  safe_mkdir("/test3");
+  safe_mkfile("/test3/a", "data");
+  safe_mkdir("/test3/d1");
+  safe_mkdir("/test3/d1/d2");
+  safe_mkdir("/test3/d1/d2/d3");
 
   // Act
-  symlink("../../../a", "/test3/d1/d2/d3/b");
+  safe_symlink("../../../a", "/test3/d1/d2/d3/b");
 
   // Assert
   char b_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     b_data[i] = 0;
-  readlink("/test3/d1/d2/d3/b", b_data);
+  safe_readlink("/test3/d1/d2/d3/b", b_data);
   if (strcmp(b_data, "../../../a") != 0)
     error(test, 1);
 
   char data[16];
   for (int i = 0; i < 16; ++i)
     data[i] = 0;
-  int fb = open("/test3/d1/d2/d3/b", O_RDONLY);
-  read(fb, data, 16);
+  int fb = safe_open("/test3/d1/d2/d3/b", O_RDONLY);
+  safe_read(fb, data, 16);
   if (strcmp(data, "data") != 0)
     error(test, 2);
-  close(fb);
+  safe_close(fb);
 
   ok(test);
 }
@@ -123,31 +174,31 @@ void test4() {
   int test = 4;
 
   // Arrange
-  mkdir("/test4");
-  mkdir("/test4/d1");
-  mkdir("/test4/d1/d2");
-  mkdir("/test4/d1/d2/d3");
-  mkfile("/test4/d1/d2/d3/a", "data");
+  safe_mkdir("/test4");
+  safe_mkdir("/test4/d1");
+  safe_mkdir("/test4/d1/d2");
+  safe_mkdir("/test4/d1/d2/d3");
+  safe_mkfile("/test4/d1/d2/d3/a", "data");
 
   // Act
-  symlink("d1/d2/d3/a", "/test4/b");
+  safe_symlink("d1/d2/d3/a", "/test4/b");
 
   // Assert
   char b_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     b_data[i] = 0;
-  readlink("/test4/b", b_data);
+  safe_readlink("/test4/b", b_data);
   if (strcmp(b_data, "d1/d2/d3/a") != 0)
     error(test, 1);
 
   char data[16];
   for (int i = 0; i < 16; ++i)
     data[i] = 0;
-  int fb = open("/test4/b", O_RDONLY);
-  read(fb, data, 16);
+  int fb = safe_open("/test4/b", O_RDONLY);
+  safe_read(fb, data, 16);
   if (strcmp(data, "data") != 0)
     error(test, 2);
-  close(fb);
+  safe_close(fb);
 
   ok(test);
 }
@@ -156,44 +207,44 @@ void test5() {
   int test = 5;
 
   // Arrange
-  mkdir("/test5");
-  mkfile("/test5/a", "data");
+  safe_mkdir("/test5");
+  safe_mkfile("/test5/a", "data");
 
   // Act
-  symlink("/test5/a", "/test5/b");
-  symlink("/test5/b", "/test5/c");
-  symlink("/test5/c", "/test5/d");
+  safe_symlink("/test5/a", "/test5/b");
+  safe_symlink("/test5/b", "/test5/c");
+  safe_symlink("/test5/c", "/test5/d");
 
   // Assert
   char b_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     b_data[i] = 0;
-  readlink("/test5/b", b_data);
+  safe_readlink("/test5/b", b_data);
   if (strcmp(b_data, "/test5/a") != 0)
     error(test, 1);
 
   char c_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     c_data[i] = 0;
-  readlink("/test5/c", c_data);
+  safe_readlink("/test5/c", c_data);
   if (strcmp(c_data, "/test5/b") != 0)
     error(test, 2);
 
   char d_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     d_data[i] = 0;
-  readlink("/test5/d", d_data);
+  safe_readlink("/test5/d", d_data);
   if (strcmp(d_data, "/test5/c") != 0)
     error(test, 3);
 
   char data[16];
   for (int i = 0; i < 16; ++i)
     data[i] = 0;
-  int fd = open("/test5/d", O_RDONLY);
-  read(fd, data, 16);
+  int fd = safe_open("/test5/d", O_RDONLY);
+  safe_read(fd, data, 16);
   if (strcmp(data, "data") != 0)
     error(test, 4);
-  close(fd);
+  safe_close(fd);
 
   ok(test);
 }
@@ -202,44 +253,44 @@ void test6() {
   int test = 6;
 
   // Arrange
-  mkdir("/test6");
-  mkfile("/test6/a", "data");
+  safe_mkdir("/test6");
+  safe_mkfile("/test6/a", "data");
 
   // Act
-  symlink("/test6/a", "/test6/b");
-  symlink("b", "/test6/c");
-  symlink("/test6/c", "/test6/d");
+  safe_symlink("/test6/a", "/test6/b");
+  safe_symlink("b", "/test6/c");
+  safe_symlink("/test6/c", "/test6/d");
 
   // Assert
   char b_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     b_data[i] = 0;
-  readlink("/test6/b", b_data);
+  safe_readlink("/test6/b", b_data);
   if (strcmp(b_data, "/test6/a") != 0)
     error(test, 1);
 
   char c_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     c_data[i] = 0;
-  readlink("/test6/c", c_data);
+  safe_readlink("/test6/c", c_data);
   if (strcmp(c_data, "b") != 0)
     error(test, 2);
 
   char d_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     d_data[i] = 0;
-  readlink("/test6/d", d_data);
+  safe_readlink("/test6/d", d_data);
   if (strcmp(d_data, "/test6/c") != 0)
     error(test, 3);
 
   char data[16];
   for (int i = 0; i < 16; ++i)
     data[i] = 0;
-  int fd = open("/test6/d", O_RDONLY);
-  read(fd, data, 16);
+  int fd = safe_open("/test6/d", O_RDONLY);
+  safe_read(fd, data, 16);
   if (strcmp(data, "data") != 0)
     error(test, 4);
-  close(fd);
+  safe_close(fd);
 
   ok(test);
 }
@@ -248,44 +299,44 @@ void test7() {
   int test = 7;
 
   // Arrange
-  mkdir("/test7");
-  mkfile("/test7/a", "data");
+  safe_mkdir("/test7");
+  safe_mkfile("/test7/a", "data");
 
   // Act
-  symlink("a", "/test7/b");
-  symlink("b", "/test7/c");
-  symlink("c", "/test7/d");
+  safe_symlink("a", "/test7/b");
+  safe_symlink("b", "/test7/c");
+  safe_symlink("c", "/test7/d");
 
   // Assert
   char b_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     b_data[i] = 0;
-  readlink("/test7/b", b_data);
+  safe_readlink("/test7/b", b_data);
   if (strcmp(b_data, "a") != 0)
     error(test, 1);
 
   char c_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     c_data[i] = 0;
-  readlink("/test7/c", c_data);
+  safe_readlink("/test7/c", c_data);
   if (strcmp(c_data, "b") != 0)
     error(test, 2);
 
   char d_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     d_data[i] = 0;
-  readlink("/test7/d", d_data);
+  safe_readlink("/test7/d", d_data);
   if (strcmp(d_data, "c") != 0)
     error(test, 3);
 
   char data[16];
   for (int i = 0; i < 16; ++i)
     data[i] = 0;
-  int fd = open("/test7/d", O_RDONLY);
-  read(fd, data, 16);
+  int fd = safe_open("/test7/d", O_RDONLY);
+  safe_read(fd, data, 16);
   if (strcmp(data, "data") != 0)
     error(test, 4);
-  close(fd);
+  safe_close(fd);
 
   ok(test);
 }
@@ -294,23 +345,23 @@ void test8() {
   int test = 8;
 
   // Arrange
-  mkdir("/test8");
+  safe_mkdir("/test8");
 
   // Act
-  symlink("b", "/test8/b");
+  safe_symlink("b", "/test8/b");
 
   // Assert
   char b_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     b_data[i] = 0;
-  readlink("/test8/b", b_data);
+  safe_readlink("/test8/b", b_data);
   if (strcmp(b_data, "b") != 0)
     error(test, 1);
 
   int fb = open("/test8/b", O_RDONLY);
   if (fb != -1) {
     error(test, 2);
-    close(fb);
+    safe_close(fb);
   }
 
   ok(test);
@@ -320,41 +371,41 @@ void test9() {
   int test = 9;
 
   // Arrange
-  mkdir("/test9");
-  mkfile("/test9/a", "data");
+  safe_mkdir("/test9");
+  safe_mkfile("/test9/a", "data");
 
   // Act
-  symlink("a", "/test9/b");
-  symlink("b", "/test9/c");
-  symlink("c", "/test9/d");
-  symlink("d", "/test9/b");
+  safe_symlink("a", "/test9/b");
+  safe_symlink("b", "/test9/c");
+  safe_symlink("c", "/test9/d");
+  safe_symlink("d", "/test9/b");
 
   // Assert
   char b_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     b_data[i] = 0;
-  readlink("/test9/b", b_data);
+  safe_readlink("/test9/b", b_data);
   if (strcmp(b_data, "d") != 0)
     error(test, 1);
 
   char c_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     c_data[i] = 0;
-  readlink("/test9/c", c_data);
+  safe_readlink("/test9/c", c_data);
   if (strcmp(c_data, "b") != 0)
     error(test, 2);
 
   char d_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     d_data[i] = 0;
-  readlink("/test9/d", d_data);
+  safe_readlink("/test9/d", d_data);
   if (strcmp(d_data, "c") != 0)
     error(test, 3);
 
   int fb = open("/test9/b", O_RDONLY);
   if (fb != -1) {
     error(test, 4);
-    close(fb);
+    safe_close(fb);
   }
 
   ok(test);
@@ -364,23 +415,23 @@ void test10() {
   int test = 10;
 
   // Arrange
-  mkdir("/test10");
+  safe_mkdir("/test10");
 
   // Act
-  symlink("/test10/a", "/test10/b");
+  safe_symlink("/test10/a", "/test10/b");
 
   // Assert
   char b_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     b_data[i] = 0;
-  readlink("/test10/b", b_data);
+  safe_readlink("/test10/b", b_data);
   if (strcmp(b_data, "/test10/a") != 0)
     error(test, 1);
 
   int fb = open("/test10/b", O_RDONLY);
   if (fb != -1) {
     error(test, 2);
-    close(fb);
+    safe_close(fb);
   }
 
   ok(test);
@@ -390,27 +441,27 @@ void test11() {
   int test = 11;
 
   // Arrange
-  mkdir("/test11");
-  mkdir("/test11/d1");
-  mkdir("/test11/d1/d2");
-  mkdir("/test11/d1/d2/d3");
-  mkfile("/test11/d1/d2/d3/a", "data");
+  safe_mkdir("/test11");
+  safe_mkdir("/test11/d1");
+  safe_mkdir("/test11/d1/d2");
+  safe_mkdir("/test11/d1/d2/d3");
+  safe_mkfile("/test11/d1/d2/d3/a", "data");
 
   // Act
-  symlink("a", "/test11/b");
+  safe_symlink("a", "/test11/b");
 
   // Assert
   char b_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     b_data[i] = 0;
-  readlink("/test11/b", b_data);
+  safe_readlink("/test11/b", b_data);
   if (strcmp(b_data, "a") != 0)
     error(test, 1);
 
   int fb = open("/test11/b", O_RDONLY);
   if (fb != -1) {
     error(test, 2);
-    close(fb);
+    safe_close(fb);
   }
 
   ok(test);
@@ -420,27 +471,27 @@ void test12() {
   int test = 12;
 
   // Arrange
-  mkdir("/test12");
-  mkdir("/test12/d1");
-  mkdir("/test12/d1/d2");
-  mkdir("/test12/d1/d2/d3");
-  mkfile("/test12/d1/d2/d3/a", "data");
+  safe_mkdir("/test12");
+  safe_mkdir("/test12/d1");
+  safe_mkdir("/test12/d1/d2");
+  safe_mkdir("/test12/d1/d2/d3");
+  safe_mkfile("/test12/d1/d2/d3/a", "data");
 
   // Act
-  symlink("../../../a", "/test12/d1/d2/d3/b");
+  safe_symlink("../../../a", "/test12/d1/d2/d3/b");
 
   // Assert
   char b_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     b_data[i] = 0;
-  readlink("/test12/d1/d2/d3/b", b_data);
+  safe_readlink("/test12/d1/d2/d3/b", b_data);
   if (strcmp(b_data, "../../../a") != 0)
     error(test, 1);
 
   int fb = open("/test12/d1/d2/d3/b", O_RDONLY);
   if (fb != -1) {
     error(test, 2);
-    close(fb);
+    safe_close(fb);
   }
 
   ok(test);
@@ -450,27 +501,27 @@ void test13() {
   int test = 13;
 
   // Arrange
-  mkdir("/test13");
-  mkfile("/test13/a", "data");
-  mkdir("/test13/d1");
-  mkdir("/test13/d1/d2");
-  mkdir("/test13/d1/d2/d3");
+  safe_mkdir("/test13");
+  safe_mkfile("/test13/a", "data");
+  safe_mkdir("/test13/d1");
+  safe_mkdir("/test13/d1/d2");
+  safe_mkdir("/test13/d1/d2/d3");
 
   // Act
-  symlink("d1/d2/d3/a", "/test13/b");
+  safe_symlink("d1/d2/d3/a", "/test13/b");
 
   // Assert
   char b_data[MAXPATH];
   for (int i = 0; i < MAXPATH; ++i)
     b_data[i] = 0;
-  readlink("/test13/b", b_data);
+  safe_readlink("/test13/b", b_data);
   if (strcmp(b_data, "d1/d2/d3/a") != 0)
     error(test, 1);
 
   int fb = open("/test13/b", O_RDONLY);
   if (fb != -1) {
     error(test, 2);
-    close(fb);
+    safe_close(fb);
   }
 
   ok(test);
@@ -551,7 +602,7 @@ view_tree(char *path, int level)
 
   if(fstat(fd, &st) < 0){
     fprintf(2, "%sview_tree: cannot stat %s\n", mar, path);
-    close(fd);
+    safe_close(fd);
     return;
   }
 
@@ -608,6 +659,6 @@ view_tree(char *path, int level)
       }
       break;
   }
-  close(fd);
+  safe_close(fd);
 }
 
